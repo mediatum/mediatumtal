f15a6da05eacbdffe8316a0fee575b6eb865e3fb
Tobias Stenzel <tobias.stenzel@tum.de>
2014-01-07 18:26:17 +0100

    macro and translation support, some fixes related to athana changes, logging

--- mediatumtal/tal.py
+++ mediatumtal/tal.py
@@ -4,11 +4,25 @@ Created on 26.07.2013
 @author: stenzel
 '''
 from __future__ import division, absolute_import
-from mediatumtal.talextracted import processTAL
+from mediatumtal import talextracted
+from mediatumtal.talextracted import runTAL, processTAL
 
 
-def getTAL(page,context,macro=None,language=None):
-    return processTAL(context,file=page, macro=macro, language=language)
+def getTAL(page, context, macro=None, language=None, request=None):
+    return processTAL(context, file=page, macro=macro, language=language, request=request)
 
-def getTALstr(string,context,macro=None,language=None,mode=None):
-    return processTAL(context,string=string, macro=macro, language=language, mode=mode)
+
+def getTALstr(string, context, macro=None, language=None, mode=None):
+    return processTAL(context, string=string, macro=macro, language=language, mode=mode)
+
+
+def set_base(basedir):
+    talextracted.setBase(basedir)
+
+
+def add_translator(translator):
+    talextracted.addTranslator(translator)
+
+
+def add_macro_resolver(macroresolver):
+    talextracted.addMacroResolver(macroresolver)
